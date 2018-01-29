7d47d41b628aa143e6c4dd4dff04be9be222a73e
Muyunyun <328375795@qq.com>
2018-01-29 10:06:45 +0800

    feat: add the config property 'scrollElem'

--- README.md
+++ README.md
@@ -35,6 +35,7 @@ window.onload = function() {
     // fixWidth: 1250,          // 容器 width: 选填：填上后会使 window.resize 失效，fixWidth 属性优先级大于 width 属性
     // width: 1250,             // 容器 width；选填: 可以设置为某个节点的宽度，默认为浏览器的宽度
     // container: 'waterfall',  // 容器 id；选填：默认为 'waterfall'
+    // scrollElem: 'content',   // 绑定 scroll 的节点 id，默认为 window
   })
 
   waterfall.on("load", function () {
--- waterfall.js
+++ waterfall.js
@@ -5,7 +5,7 @@
     this.sub = {}
   }
 
-  eventEmitter.prototype.on = function(eventName, func) {
+  eventEmitter.prototype.on = function (eventName, func) {
     if (this.sub[eventName]) { // 一种类型的事件只能绑定一个函数
       return
     }
@@ -15,7 +15,7 @@
     this.sub[eventName].push(func)
   }
 
-  eventEmitter.prototype.emit = function(eventName) {
+  eventEmitter.prototype.emit = function (eventName) {
     const argsList = Array.prototype.slice.call(arguments, 1)
     for (let i = 0, length = this.sub[eventName].length; i < length; i++) {
       this.sub[eventName][i].apply(this, argsList)
@@ -30,6 +30,7 @@
       width: `${options.width || document.body.clientWidth || document.documentElement.clientWidth}px`,
       container: options.container || 'waterfall',
       resize: false,
+      scrollElem: options.scrollElem,
     }
     this.init(options) // 这个 this 是 new 的时候，绑上去的
     this.bind()
@@ -40,14 +41,14 @@
 
   const proto = Waterfall.prototype
 
-  proto.compareOpts = function(options) {
+  proto.compareOpts = function (options) {
     const self = this
     Object.keys(options).forEach((key) => {
       self.opts[key] = options[key]
     })
   }
 
-  proto.init = function(options) {
+  proto.init = function (options) {
     this.compareOpts(options)
     const $waterfall = document.getElementById(this.opts.container)
     $waterfall.style.width = `${this.opts.fixWidth || this.opts.width}px`
@@ -80,7 +81,7 @@
     }
   }
 
-  proto.getPerNum = function() {
+  proto.getPerNum = function () {
     const $waterfall = document.getElementById(this.opts.container)
     const imgList = $waterfall.getElementsByTagName('img')
     const singleImgWidth = imgList[0].offsetWidth // 瀑布流默认每张图片宽度相等
@@ -88,7 +89,7 @@
   }
 
   // 在 init 函数基础上的优化，触发 scroll 的时候，只对增加的部分渲染，这个函数想了比较久。大体思路首先找到最后一列的图片(注意不一定连续的)，并在高度最小的那个图片后面添加图片。
-  proto.append = function() {
+  proto.append = function () {
     const $waterfall = document.getElementById(this.opts.container)
     const imgList = $waterfall.getElementsByTagName('img')
     const length = imgList.length
@@ -122,16 +123,23 @@
     }
   }
 
-  proto.bind = function() {
+  proto.bind = function () {
     // 如果设置了 fixWidth 的长度，则没必要绑定 resize
     if (!this.opts.fixWidth) {
-      util.addEventListener('resize', resize.bind(this))
+      util.addEventListener(window, 'resize', resize.bind(this))
     }
-    util.addEventListener('scroll', scroll.bind(this))
+    const bindScrollElem = document.getElementById(this.opts.scrollElem)
+    util.addEventListener(bindScrollElem || window, 'scroll', scroll.bind(this))
+  }
+
+  // 解除对 scroll 的绑定
+  proto.unbind = function () {
+    const bindScrollElem = document.getElementById(this.opts.scrollElem)
+    util.removeEventListener(bindScrollElem || window, 'scroll', scroll.bind(this))
   }
 
   // 获取最小高度的数组下标
-  proto.getMinPointer = function(perList) {
+  proto.getMinPointer = function (perList) {
     const minHeight = Math.min.apply(null, perList)
     for (let i in perList) {
       if (perList[i] === minHeight) {
@@ -141,7 +149,7 @@
   }
 
   // 重置 position: absolute 防止 onresize 的时候产生干扰
-  proto.resize = function(imgList) {
+  proto.resize = function (imgList) {
     for (let i = 0; i < imgList.length; i++) {
       imgList[i].style.position = 'static'
     }
@@ -149,9 +157,9 @@
   }
 
   // 注册一次 done 事件，并触发
-  proto.done = function() {
+  proto.done = function () {
     const self = this
-    this.on('done', function() {
+    this.on('done', function () {
       isLoading = false
       // 取消加载按钮
       const loadContainer = document.getElementsByClassName('waterfall-loading')[0]
@@ -162,7 +170,7 @@
   }
 
   // 添加加载动画
-  proto.loadImg = function() {
+  proto.loadImg = function () {
     const $waterfall = document.getElementById(this.opts.container)
     const loadContainer = document.createElement('div')
     loadContainer.setAttribute('class', 'waterfall-loading')
@@ -177,23 +185,41 @@
     })
   }
 
-  const scroll = function() {
-    if (isLoading) return false // 避免一次触发事件多次
-    const $waterfall = document.getElementById(this.opts.container)
-    const imgList = $waterfall.getElementsByTagName('img')
-    const scrollPX = document.body.scrollTop || document.documentElement.scrollTop
-    const bsHeight = document.body.clientHeight || document.documentElement.clientHeight
-    if (scrollPX + bsHeight > imgList[imgList.length - 1].offsetTop) { // 浏览器高度 + 滚动距离 < 最后一张图片的 offsetTop
-      isLoading = true
-      this.loadImg()
-      this.emit('load')
+  const scroll = function () {
+    // 如果绑定在 window 上，
+    if (!this.opts.scrollElem) {
+      if (isLoading) return false // 避免一次触发事件多次
+      const $waterfall = document.getElementById(this.opts.container)
+      const imgList = $waterfall.getElementsByTagName('img')
+      const scrollPX = document.body.scrollTop || document.documentElement.scrollTop
+      const bsHeight = document.body.clientHeight || document.documentElement.clientHeight
+      if (scrollPX + bsHeight > imgList[imgList.length - 1].offsetTop) { // 浏览器高度 + 滚动距离 < 最后一张图片的 offsetTop
+        isLoading = true
+        this.loadImg()
+        this.emit('load')
+      }
+    } else { // scrollElem 绑定在其它节点上
+      if (isLoading) return false
+      const $waterfall = document.getElementById(this.opts.container)
+      const imgList = $waterfall.getElementsByTagName('img')
+      const parentElem = document.getElementById(this.opts.scrollElem)
+      const scrollElemPX = parentElem.scrollTop
+      const parentElemHeight = parentElem.clientHeight
+      if (scrollElemPX + parentElemHeight > imgList[imgList.length - 1].offsetTop) {
+        isLoading = true
+        this.loadImg()
+        this.emit('load')
+      }
     }
   }
 
   const util = {
-    addEventListener: function (evName, func) {
-      window.addEventListener(evName, func, false)
-    }
+    addEventListener: function (elem, evName, func) {
+      elem.addEventListener(evName, func, false)
+    },
+    removeEventListener: function (elem, evName, func) {
+      elem.removeEventListener(evName, func, false)
+    },
   }
 
   window.Waterfall = Waterfall
